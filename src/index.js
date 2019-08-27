04170cc3894ce9e724d1ca9723ca403b1505f54a
Henry Lay <henry88lay@gmail.com>
2019-08-27 14:40:10 +1000

    setting up the middleware for the project

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import rootReducer from './reducers/index';
 
+const store = createStore(rootReducer);
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
