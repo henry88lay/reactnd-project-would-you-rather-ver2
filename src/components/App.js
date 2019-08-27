51d96ef4a78ab2e65cc6f823eb55d5e34b0e6c28
Henry Lay <henry88lay@gmail.com>
2019-08-27 15:05:04 +1000

    setting up the app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,29 @@
 import React, {Component, Fragment} from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import {Grid} from 'semantic-ui-react';
-import Nav from './Nav';
+import {handleInitialData} from '../actions/shared';
+import {connect} from 'react-redux';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div className='App'>
-          <Fragment>
-            <Nav />
-          </Fragment>
-        </div>
+        <div className='App'></div>
       </Router>
     );
   }
 }
 
-export default App;
+const ContentGrid = ({children}) => (
+  <Grid padded='vertically' columns={1} centered>
+    <Grid.Row>
+      <Grid.Column style={{maxWidth: 550}}>{children}</Grid.Column>
+    </Grid.Row>
+  </Grid>
+);
+
+export default connect(
+  null,
+  {handleInitialData}
+)(App);
