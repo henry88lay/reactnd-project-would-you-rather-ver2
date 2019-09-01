a1a1b0a983638e511971c4b4c06da8d4011ffdcb
Henry Lay <henry88lay@gmail.com>
2019-09-01 19:34:38 +1000

    letting up thje login.js for componenets

---
+++ src/components/Login.js
@@ -0,0 +1,19 @@
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {
+  Segment,
+  Grid,
+  Header,
+  Image
+}
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
